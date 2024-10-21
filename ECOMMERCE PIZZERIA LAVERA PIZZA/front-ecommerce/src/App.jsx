0bcff31f7c0b95edb8d92c8f06042b958fe634d6
Gonzalo <34680110ax@gmail.com>
2024-10-21 20:42:54 -0300

    contact almost finished rest map iframe

--- ECOMMERCE PIZZERIA LAVERA PIZZA/front-ecommerce/src/App.jsx
+++ ECOMMERCE PIZZERIA LAVERA PIZZA/front-ecommerce/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import MenuPage from './pages/MenuPage';
 import {HomePage} from './pages/HomePage';
+import ContactPAge from './pages/ContactPAge';
 
 function App() {
   //comentario random
@@ -8,6 +9,8 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/menu' element={<MenuPage/>}/>
+      <Route path='/contact' element={<ContactPAge/>}/>
+      <Route path='*' element={<h1>Not Found</h1>}/>
     </Routes>
   )
 }
