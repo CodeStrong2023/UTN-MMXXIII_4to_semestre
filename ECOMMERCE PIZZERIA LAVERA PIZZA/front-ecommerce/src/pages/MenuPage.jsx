30c9731aab92d208314fffb5fa76a23951c36afe
Franco Fiuri <franco.fiuri@gmail.com>
2024-11-03 19:14:15 -0300

    Add Responsive Page Menu

--- ECOMMERCE PIZZERIA LAVERA PIZZA/front-ecommerce/src/pages/MenuPage.jsx
+++ ECOMMERCE PIZZERIA LAVERA PIZZA/front-ecommerce/src/pages/MenuPage.jsx
@@ -3,6 +3,7 @@ import {Section1Menu} from '../components/Menu/Section1Menu'
 import {Section2Menu} from '../components/Menu/Section2Menu'
 import {Section3Menu} from '../components/Menu/Section3Menu'
 import {Section4Menu} from '../components/Menu/Section4Menu'
+import {Section5Menu} from '../components/Menu/Section5Menu'
 
 export const MenuPage = () => {
   return (
@@ -10,6 +11,7 @@ export const MenuPage = () => {
       <Section1Menu />
       <Section2Menu />
       <Section3Menu />
+      <Section5Menu />
       <Section4Menu />
     </div>
   )
